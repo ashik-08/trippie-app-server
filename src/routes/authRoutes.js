dc70c2ef7c2abb4f13fa09206037c0c5de8cc98a
ashik-08 <ashikurrahman.8october@gmail.com>
2024-11-16 22:37:10 +0600

    [feat](token): add refresh token route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -21,14 +21,16 @@ router.post("/login", async (req, res) => {
     console.log("from /api/auth/login -- refreshToken:", refreshToken);
     res
       .cookie("accessToken", accessToken, {
-        httpOnly: true,
+        httpOnly: false,
         secure: process.env.NODE_ENV === "production" ? true : false,
         sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        maxAge: 2 * 60 * 60 * 1000,
       })
       .cookie("refreshToken", refreshToken, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production" ? true : false,
+        httpOnly: false,
+        secure: process.env.NODE_ENV === "production",
         sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
       })
       .send({ success: true });
   } catch (error) {
@@ -73,12 +75,13 @@ router.post("/refresh-token", async (req, res) => {
         return res.status(403).send({ message: "Invalid refresh token" });
       }
 
-      const accessToken = generateAccessToken({ email: user.email });
+      const newAccessToken = generateAccessToken({ email: user.email });
       res
-        .cookie("accessToken", accessToken, {
-          httpOnly: true,
+        .cookie("accessToken", newAccessToken, {
+          httpOnly: false,
           secure: process.env.NODE_ENV === "production" ? true : false,
           sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+          maxAge: 2 * 60 * 60 * 1000,
         })
         .send({ success: true });
     });
