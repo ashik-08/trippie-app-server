eba3536f58ad52b595d6d72a932745d88389bb6b
ashik-08 <ashikurrahman.8october@gmail.com>
2024-12-14 02:23:41 +0600

    [feat](appointment): add update and delete functionality for appointments

--- src/routes/appointmentRoutes.js
+++ src/routes/appointmentRoutes.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const Appointment = require("../models/Appointment");
 const Guide = require("../models/Guide");
+const verifyToken = require("../middlewares/verifyToken");
+const verifyRole = require("../middlewares/verifyRole");
 const sendAppointmentNotificationEmail = require("../services/guideEmailService");
+const sendAppointmentEmail = require("../services/appointmentEmailService");
 
 // Create a new appointment
 router.post("/", async (req, res) => {
@@ -44,5 +47,67 @@ router.post("/", async (req, res) => {
     res.status(500).send({ message: "Internal Server Error" });
   }
 });
+
+// Update appointment status
+router.put(
+  "/:appointmentId",
+  verifyToken,
+  verifyRole(["tour-guide"]),
+  async (req, res) => {
+    const { appointmentId } = req.params;
+    const { status } = req.body;
+
+    try {
+      const appointment = await Appointment.findById(appointmentId);
+      appointment.status = status;
+      await appointment.save();
+      // update the guide's appointments array
+      const guide = await Guide.findById(appointment.guideId);
+      const appointmentIndex = guide.appointments.findIndex(
+        (appointment) => appointment.appointmentId === appointmentId
+      );
+      guide.appointments[appointmentIndex].status = status;
+      await guide.save();
+
+      // Send notification email to the user and guide
+      await sendAppointmentEmail(appointment, status);
+
+      res.status(200).send(guide);
+    } catch (error) {
+      console.log("Failed to update appointment status", error);
+      res.status(500).send({ message: "Internal Server Error" });
+    }
+  }
+);
+
+// Delete appointment by id
+router.delete(
+  "/:appointmentId",
+  verifyToken,
+  verifyRole(["tour-guide"]),
+  async (req, res) => {
+    const { appointmentId } = req.params;
+
+    try {
+      const appointment = await Appointment.findByIdAndDelete(appointmentId);
+      if (!appointment) {
+        return res.status(200).send({ message: "Appointment not found" });
+      }
+
+      // Update the guide's appointments array
+      const guide = await Guide.findById(appointment.guideId);
+      const appointmentIndex = guide.appointments.findIndex(
+        (appointment) => appointment.appointmentId === appointmentId
+      );
+      guide.appointments.splice(appointmentIndex, 1);
+      await guide.save();
+
+      res.status(200).send(guide);
+    } catch (error) {
+      console.log("Failed to delete appointment", error);
+      res.status(500).send({ message: "Internal Server Error" });
+    }
+  }
+);
 
 module.exports = router;
